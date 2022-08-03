3f02e08a560c9e33c22ea226e28c5c3029a47cdd
Ben316clark <ben316clark@comcast.net>
2022-08-02 18:17:41 -0700

    Main changes

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,76 @@
-// Adding a Single line comment 
+function pickColorFamily(){
+     x = Math.random() * 190;
+     console.log(x );
+if( x < 30)
+console.log('red');
+color = red
+else if(x >= 30 && x < 60 )
+console.log('blue');
+ color = red
+else if(x >= 60 && x < 90 )
+console.log('yellow');
+let color = red;
+else if(x >= 90 && x < 120 )
+console.log('green');
+let color = red;
+else if(x >= 120 && x < 150 )
+console.log('purple');
+let color = red;
+else if(x >= 150 && x < 180 )
+console.log('pink');
+let color = red;
+else if(x >= 180 && x < 185 )
+console.log('Black'); 
+let color = red;
+else if(x >= 185 && x < 190 )
+console.log('white');   
+let color = red;
+}
+function changeDivColor(){
+    var element = document.getElementById("ColorDiv");
+    element.style.backgroundColor = color1;
+}
+function changeRevealColor(){
+    var element = document.getElementByyId("Color");
+    element.style.color = color
+}
+function pickColorFromArray(){
+    let i = math.Floor(Math.random()*15) ;
+    if (i = 1)
+    else if(i = 2)
+    else if(i = 3)
+    else if(i = 4)
+    else if(i = 5)
+    else if(i = 6)
+    else if(i = 7)
+    else if(i = 8)
+    else if(i = 9)
+    else if(i = 10)
+    else if(i = 11)
+    else if(i = 12)
+    else if(i = 13)
+    else if(i = 14)
+    else if(i = 15)
+   
+}
+// red
+let arrayRed = array: ['rgb(100, 0, 0)','rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', 'rgb(100, 0, 0)', ]
+// blue
+let arrayBlue = array: ['rgb(0, 224, 237)','rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', 'rgb(0, 224, 237)', ]
+// yellow
+let arrayYellow = array: ['rgb(161, 150, 0)','rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', 'rgb(161, 150, 0)', ]
+// green
+let arrayGreen = array: ['rgb(0, 20, 0)','rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', 'rgb(0, 20, 0)', ]
+// purple
+let arrayPurple = array: ['rgb(28, 0, 13)','rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', 'rgb(28, 0, 13)', ]
+// pink
+let arrayPink = array: ['rgb(170, 0, 63)','rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', 'rgb(170, 0, 63)', ]
+// black
+ let black = 'rgb(0,0,0)' 
+// white
+let white = 'rgb(255,255,255)'
 
-/* Adding a Multi-line Comment 
 
-See */
-
-console.log("Benjamin Clark");
-console.log("Sushi");
-
-
+pickColorFamily()
+changeRevealColor()
+changeDivColor()
